d5ab22c850019e4dec8f5ac8d038bd7c756d996b
asbhogal <asbhogal1@live.co.uk>
2022-10-07 21:43:46 +0100

    updated script notes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// math.floor values
+// math.round values
 // use .fetch()
-// headers not required - no POST
+// check if headers required
 
 const baseCryptoSrc = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2CBNB%2CUSD%20coin%2CXRP%2CBinance%20USD%2CCardano%2CSolana%2CDogecoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true";
 
