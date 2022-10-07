dcd860b0cbe9665533febc450d4e810f1df2cfe4
asbhogal <asbhogal1@live.co.uk>
2022-10-08 00:43:31 +0100

    added to light-dark toggle script

--- toggle-button.js
+++ toggle-button.js
@@ -2,12 +2,20 @@ const body = document.querySelector('body');
 const btnToggle = document.querySelector('.btn-icon-container');
 const modeIcon = document.querySelector('.mode-icon');
 
+document.addEventListener("DOMContentLoaded", function() {
+    
+});
+
 btnToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
+    modeIcon.classList.add('icon-transitions');
+    modeIcon.classList.add('spin-animations')
 
     if(body.classList.contains('dark-mode')) {
         document.querySelector('.mode-icon').innerHTML = "dark_mode";
+        document.querySelector('.mode-icon').style.color = "#000";
     } else {
         document.querySelector('.mode-icon').innerHTML = "light_mode";
+        document.querySelector('.mode-icon').style.color = "#fac213";
     }
-})
+});
