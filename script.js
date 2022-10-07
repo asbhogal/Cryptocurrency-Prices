a22881089fb513687fa96ddbaba34fdd1c7800ae
asbhogal <asbhogal1@live.co.uk>
2022-10-07 18:11:28 +0100

    fetched API and outputted to console (as JSON)

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // math.floor values
 // use .fetch()
+
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2CBNB%2CUSD%20coin%2CXRP%2CBinance%20USD%2CCardano%2CSolana%2CDogecoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true')
+    .then(res => res.json())
+    .then(data => console.log(data))
